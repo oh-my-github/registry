899e5ae5f4116be0841be43af213ca4562cee696
1ambda <1amb4a@gmail.com>
2016-03-27 11:44:34 +0900

    chore: Serving registry.json
    - `/resource/profile/registry.json`

--- tools/build.js
+++ tools/build.js
@@ -1,45 +1,43 @@
-// More info on Webpack's Node API here: https://webpack.github.io/docs/node.js-api.html
-// Allowing console calls below since this is a build file.
-/*eslint-disable no-console */
-import webpack from 'webpack';
-import webpackConfigBuilder from '../webpack.config';
-import colors from 'colors';
-import { argv as args } from 'yargs';
+import webpack from 'webpack'
+import webpackConfigBuilder from '../webpack.config'
+import colors from 'colors'
+import { argv as args, } from 'yargs'
 
-process.env.NODE_ENV = 'production'; // this assures React is built in prod mode and that the Babel dev config doesn't apply.
+process.env.NODE_ENV = 'production'
 
-const webpackConfig = webpackConfigBuilder(process.env.NODE_ENV);
+const webpackConfig = webpackConfigBuilder(process.env.NODE_ENV)
 
 webpack(webpackConfig).run((err, stats) => {
-  const inSilentMode = args.s; // set to true when -s is passed on the command
+  /*eslint-disable no-console */
+  const inSilentMode = args.s // set to true when -s is passed on the command
 
   if (!inSilentMode) {
-    console.log('Generating minified bundle for production use via Webpack...'.bold.blue);
+    console.log('Generating minified bundle for production use via Webpack...'.bold.blue)
   }
 
   if (err) { // so a fatal error occurred. Stop here.
-    console.log(err.bold.red);
+    console.log(err.bold.red)
 
-    return 1;
+    return 1
   }
 
-  const jsonStats = stats.toJson();
+  const jsonStats = stats.toJson()
 
   if (jsonStats.hasErrors) {
-    return jsonStats.errors.map(error => console.log(error.red));
+    return jsonStats.errors.map(error => console.log(error.red))
   }
 
   if (jsonStats.hasWarnings && !inSilentMode) {
-    console.log('Webpack generated the following warnings: '.bold.yellow);
-    jsonStats.warnings.map(warning => console.log(warning.yellow));
+    console.log('Webpack generated the following warnings: '.bold.yellow)
+    jsonStats.warnings.map(warning => console.log(warning.yellow))
   }
 
   if (!inSilentMode) {
-    console.log(`Webpack stats: ${stats}`);
+    console.log(`Webpack stats: ${stats}`)
   }
 
-  // if we got this far, the build succeeded.
-  console.log('Your app has been compiled in production mode and written to /dist. It\'s ready to roll!'.green.bold);
+  console.log('Your app has been compiled in production mode and written to /dist. It\'s ready to roll!'.green.bold)
+  /*eslint-enable no-console */
 
-  return 0;
-});
+  return 0
+})
--- webpack.config.js
+++ webpack.config.js
@@ -1,84 +1,96 @@
-// For info about this file refer to webpack and webpack-hot-middleware documentation
-// Rather than having hard coded webpack.config.js for each environment, this
-// file generates a webpack config for the environment passed to the getConfig method.
-import webpack from 'webpack';
-import path from 'path';
-import ExtractTextPlugin from 'extract-text-webpack-plugin';
+import webpack from 'webpack'
+import path from 'path'
+import ExtractTextPlugin from 'extract-text-webpack-plugin'
 
-const developmentEnvironment = 'development' ;
-const productionEnvironment = 'production';
-const testEnvironment = 'test';
+const developmentEnvironment = 'development'
+const productionEnvironment = 'production'
+const testEnvironment = 'test'
 
 const getPlugins = function (env) {
   const GLOBALS = {
     'process.env.NODE_ENV': JSON.stringify(env),
-    __DEV__: env === developmentEnvironment
-  };
+    __DEV__: env === developmentEnvironment,
+  }
 
   const plugins = [
     new webpack.optimize.OccurenceOrderPlugin(),
-    new webpack.DefinePlugin(GLOBALS) //Tells React to build in prod mode. https://facebook.github.io/react/downloads.html
-  ];
+    new webpack.DefinePlugin(GLOBALS),
+  ]
 
   switch (env) {
     case productionEnvironment:
-      plugins.push(new ExtractTextPlugin('styles.css'));
-      plugins.push(new webpack.optimize.DedupePlugin());
-      plugins.push(new webpack.optimize.UglifyJsPlugin());
-      break;
+      plugins.push(new ExtractTextPlugin('styles.css'))
+      plugins.push(new webpack.optimize.DedupePlugin())
+      plugins.push(new webpack.optimize.UglifyJsPlugin())
+      break
 
     case developmentEnvironment:
-      plugins.push(new webpack.HotModuleReplacementPlugin());
-      plugins.push(new webpack.NoErrorsPlugin());
-      break;
+      plugins.push(new webpack.HotModuleReplacementPlugin())
+      plugins.push(new webpack.NoErrorsPlugin())
+      break
   }
 
-  return plugins;
-};
+  return plugins
+}
 
 const getEntry = function (env) {
-  const entry = [];
+  const entry = []
 
-  if (env === developmentEnvironment ) { // only want hot reloading when in dev.
-    entry.push('webpack-hot-middleware/client');
+  if (env === developmentEnvironment ) {
+    entry.push('webpack-hot-middleware/client')
   }
 
-  entry.push('./src/index');
+  entry.push('./src/index')
 
-  return entry;
-};
+  return entry
+}
 
 const getLoaders = function (env) {
-  const loaders = [{ test: /\.js$/, include: path.join(__dirname, 'src'), loaders: ['babel', 'eslint'] },
-                   { test: /\.(jpe?g|png|gif|svg)$/i, loaders: ['file']}];
+  const loaders = [
+    { test: /\.js$/,
+      include: path.join(__dirname, 'src'),
+      loaders: ['babel', 'eslint',], },
+    { test: /\.(jpe?g|png|gif|svg)$/i,
+      loaders: ['file',],
+    },
+  ]
 
   if (env === productionEnvironment ) {
-    // generate separate physical stylesheet for production build using ExtractTextPlugin. This provides separate caching and avoids a flash of unstyled content on load.
-    loaders.push({test: /(\.css|\.scss)$/, loader: ExtractTextPlugin.extract("css?sourceMap!sass?sourceMap")});
+    loaders.push(
+      {
+        test: /(\.css|\.scss)$/,
+        loader: ExtractTextPlugin.extract('css?sourceMap!sass?sourceMap'),
+      }
+    )
   } else {
-    loaders.push({test: /(\.css|\.scss)$/, loaders: ['style', 'css?sourceMap', 'sass?sourceMap']});
+    loaders.push(
+      {
+        test: /(\.css|\.scss)$/,
+        loaders: ['style', 'css?sourceMap', 'sass?sourceMap',],
+      }
+    )
   }
 
-  return loaders;
-};
+  return loaders
+}
 
 function getConfig(env) {
   return {
     debug: true,
-    devtool: env === productionEnvironment  ? 'source-map' : 'cheap-module-eval-source-map', // more info:https://webpack.github.io/docs/build-performance.html#sourcemaps and https://webpack.github.io/docs/configuration.html#devtool
-    noInfo: true, // set to false to see a list of every file being bundled.
+    devtool: env === productionEnvironment  ? 'source-map' : 'cheap-module-eval-source-map',
+    noInfo: true,
     entry: getEntry(env),
-    target: env === testEnvironment ? 'node' : 'web', // necessary per https://webpack.github.io/docs/testing.html#compile-and-test
+    target: env === testEnvironment ? 'node' : 'web',
     output: {
-      path: __dirname + '/dist', // Note: Physical files are only output by the production build task `npm run build`.
+      path: __dirname + '/dist',
       publicPath: '',
-      filename: 'bundle.js'
+      filename: 'bundle.js',
     },
     plugins: getPlugins(env),
     module: {
-      loaders: getLoaders(env)
-    }
-  };
+      loaders: getLoaders(env),
+    },
+  }
 }
 
-export default getConfig;
+export default getConfig
