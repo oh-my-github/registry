877e0c4e1d82ed77029ce1b6a2f5d6747c1a6ee2
njir <njir@naver.com>
2016-05-08 10:30:35 +0900

    chore: Delete css on index.html

--- tools/buildHtml.js
+++ tools/buildHtml.js
@@ -10,8 +10,6 @@ fs.readFile('src/index.html', 'utf8', (err, markup) => {
 
   const $ = cheerio.load(markup)
 
-  $('head').prepend('<link rel="stylesheet" href="styles.css">')
-
   fs.writeFile('dist/index.html', $.html(), 'utf8', function (err) {
     if (err) {
       return console.log(err)
